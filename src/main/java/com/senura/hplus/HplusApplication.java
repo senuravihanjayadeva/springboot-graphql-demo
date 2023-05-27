16dbd76a6428dee69849e97b3b2a667e543d2641
senuravihanjayadeva <senurajayadeva@gmail.com>
2023-05-27 15:12:07 +0530

    inital commit

---
+++ src/main/java/com/senura/hplus/HplusApplication.java
@@ -0,0 +1,13 @@
+package com.senura.hplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HplusApplication.class, args);
+	}
+
+}
