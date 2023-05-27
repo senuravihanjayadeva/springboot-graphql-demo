841a8a569cb86706a1791335459c06b40a0a87c1
senuravihanjayadeva <senurajayadeva@gmail.com>
2023-05-27 17:53:07 +0530

    Implemented query mappings in Customer controller

--- src/main/java/com/senura/hplus/controller/CustomerController.java
+++ src/main/java/com/senura/hplus/controller/CustomerController.java
@@ -1,2 +1,29 @@
-package com.senura.hplus.controller;public class CustomerController {
+package com.senura.hplus.controller;
+
+import com.senura.hplus.data.Customer;
+import com.senura.hplus.data.CustomerRepository;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CustomerController {
+    private final CustomerRepository customerRepository;
+
+    public CustomerController(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @QueryMapping
+    public Iterable<Customer> customers(){
+        return this.customerRepository.findAll();
+    }
+    @QueryMapping
+    public Customer customerById(@Argument Long id){
+        return this.customerRepository.findById(id).orElseThrow();
+    }
+    @QueryMapping
+    public Customer customerByEmail(@Argument String email){
+        return this.customerRepository.findCustomerByEmail(email);
+    }
 }
