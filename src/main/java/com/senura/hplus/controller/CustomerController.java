0b883f300efccc14db31b012c56339505c83bef4
senuravihanjayadeva <senurajayadeva@gmail.com>
2023-05-28 10:07:29 +0530

    Allow cross origin

--- src/main/java/com/senura/hplus/controller/CustomerController.java
+++ src/main/java/com/senura/hplus/controller/CustomerController.java
@@ -5,9 +5,11 @@ import com.senura.hplus.data.CustomerRepository;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@CrossOrigin("*")
+@RestController
 public class CustomerController {
     private final CustomerRepository customerRepository;
 
@@ -27,7 +29,6 @@ public class CustomerController {
     public Customer customerByEmail(@Argument String email){
         return this.customerRepository.findCustomerByEmail(email);
     }
-
     @MutationMapping
     public Customer addCustomer(@Argument(name = "input") CustomerInput customerInput){
         return this.customerRepository.save(customerInput.getCustomerEntity());
